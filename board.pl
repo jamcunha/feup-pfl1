7db582e425e89c9df31f6cf95907aeb7da184713
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2023-10-31 21:58:42 +0000

    feat: display board

--- board.pl
+++ board.pl
@@ -1,4 +1,15 @@
-% r -> Red, b -> Blue, e -> Empty
+% Board representation
+%
+%    | 0 | 1 | 2 | 3 |
+% --------------------
+%  0 | R | B | R | B |
+% --------------------
+%  1 | B | R | B | R |
+% --------------------
+%  2 | R | B | R | B |
+% --------------------
+%  3 | B | R | B | R |
+% --------------------
 
 /**
  * initial_board/1
@@ -8,8 +19,60 @@
  */
 initial_board(Board) :-
     Board = [
-        [r, b, r, b],
-        [b, r, b, r],
-        [r, b, r, b],
-        [b, r, b, r]
+        [red, blue, red, blue],
+        [blue, red, blue, red],
+        [red, blue, red, blue],
+        [blue, red, blue, red]
     ].
+
+/**
+ * symbol/2
+ * symbol(+Piece, -Symbol)
+ *
+ * Predicate that associates symbols ready to be printed to board
+ */
+symbol(red, 'R').
+symbol(blue, 'B').
+symbol(empty, ' ').
+
+/**
+ * display_board/1
+ * display_board(+Board)
+ *
+ * Predicate that displays the board
+ */
+display_board(Board) :-
+    write('   | 0 | 1 | 2 | 3 |\n'),
+    write('--------------------\n'),
+    display_board(Board, 0).
+
+/**
+ * display_board/2
+ * display_board(+Board, +N)
+ *
+ * Auxiliary predicate to displays the board
+ * N is the number of the row
+ */
+display_board([], _).
+display_board([Row|Rest], N) :-
+    write(' '),
+    write(N),
+    write(' | '),
+    display_row(Row),
+    write('\n'),
+    write('--------------------\n'),
+    N1 is N + 1,
+    display_board(Rest, N1).
+
+/**
+ * display_row/1
+ * display_row(+Row)
+ *
+ * Auxiliary predicate to displays a row of the board
+ */
+display_row([]).
+display_row([Piece|Rest]) :-
+    symbol(Piece, Symbol),
+    write(Symbol),
+    write(' | '),
+    display_row(Rest).
--- utils.pl
+++ utils.pl
@@ -46,3 +46,50 @@ read_number_between(Min, Max, X) :-
  * Clears the screen.
  */
 clear :- write('\033[2J\033[1;1H').
+
+/**
+ * list_get/3
+ * list_get(+List, +Index, -Value)
+ *
+ * Gets the element at Index in List.
+ */
+list_get(List, Index, Value) :-
+    nth0(Index, List, Value).
+
+/**
+ * matrix_get/4
+ * matrix_get(+Matrix, +Row, +Col, -Value)
+ *
+ * Gets the element at (Row, Col) in Matrix.
+ */
+matrix_get([_|T], Row, Col, Value) :-
+    Row > 0,
+    Row1 is Row - 1, !,
+    matrix_get(T, Row1, Col, Value).
+
+matrix_get([H|_], 0, Col, Value) :-
+    list_get(H, Col, Value), !.
+
+/**
+ * list_replace/4
+ * list_replace(+List, +Index, +Value, -Result)
+ *
+ * Replaces the element at Index in List with Value.
+ */
+list_replace(List, Index, Value, Result) :-
+    nth0(Index, List, _, Temp),
+    nth0(Index, Result, Value, Temp).
+
+/**
+ * matrix_replace/5
+ * matrix_replace(+Matrix, +Row, +Col, +Value, -Result)
+ *
+ * Replaces the element at (Row, Col) in Matrix with Value.
+ */
+matrix_replace([H|T], Row, Col, Value, [H|TR]) :-
+    Row > 0,
+    Row1 is Row - 1, !,
+    matrix_replace(T, Row1, Col, Value, TR).
+
+matrix_replace([H|T], 0, Col, Value, [HR|T]) :-
+    list_replace(H, Col, Value, HR), !.
