cd35eb92d6080dcd3f2072e82868db6d20e5748a
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2023-11-01 15:45:07 +0000

    feat: game continues after validation

--- game.pl
+++ game.pl
@@ -9,10 +9,15 @@
 % against -> human, computer
 
 start_game(Turn-Against) :-
-    clear,
     initial_board(Board),
-    % have a predicate to start the game accounting against
-    format('Turn: ~w~n', [Turn]), nl,
+    new_turn(Board, Turn-Against).
+
+switch_turn(blue, red).
+switch_turn(red, blue).
+
+new_turn(Board, Turn-Against) :-
+    clear,
+    format('Turn: ~w', [Turn]), nl, nl,
     display_board(Board), nl,
     repeat,
     write('Choose a Row: '), read_number_between(0, 3, Row),
@@ -21,8 +26,11 @@ start_game(Turn-Against) :-
     (Checker = Turn ->
         nl, write('Correct Checker is working, validate moves from here'), nl, nl,
         repeat,
-        (move(Row, Column) ->
-            nl, write('Move is valid'), nl
+        (validate_move(Row, Column, MoveRow, MoveColumn) ->
+            nl, write('Move is valid'), nl,
+            execute_move(Board, Row, Column, MoveRow, MoveColumn, NewBoard),
+            switch_turn(Turn, NewTurn),
+            new_turn(NewBoard, NewTurn-Against)
         ;
             nl, write('Move is invalid, Choose another move'), nl, nl, fail
         )
@@ -30,15 +38,24 @@ start_game(Turn-Against) :-
         nl, write('Invalid checker, Pick another coordinate'), nl, nl, fail
     ).
 
-move(Row, Column) :-
-    write('Choose a Row: '), read_number_between(0, 3, MoveRow),
-    write('Choose a Column: '), read_number_between(0, 3, MoveColumn),
-    validate_move(Row, Column, MoveRow, MoveColumn).
-    %? execute_move(Row, Column, MoveRow, MoveColumn)
 
 validate_move(Row, Column, MoveRow, MoveColumn) :-
+    write('Choose a Row: '), read_number_between(0, 3, MoveRow),
+    write('Choose a Column: '), read_number_between(0, 3, MoveColumn),
     % still missing validation about groups (maybe have a future board to check)
-    MoveRow =:= Row + 1, MoveColumn =:= Column;
-    MoveRow =:= Row - 1, MoveColumn =:= Column;
-    MoveRow =:= Row, MoveColumn =:= Column + 1;
-    MoveRow =:= Row, MoveColumn =:= Column - 1.
+    (
+        MoveRow =:= Row + 1, MoveColumn =:= Column
+    ;
+        MoveRow =:= Row - 1, MoveColumn =:= Column
+    ;
+        MoveRow =:= Row, MoveColumn =:= Column + 1
+    ;
+        MoveRow =:= Row, MoveColumn =:= Column - 1
+    ).
+
+execute_move(Board, Row, Column, MoveRow, MoveColumn, NewBoard) :-
+    format('Moving from ~w, ~w to ~w, ~w', [Row, Column, MoveRow, MoveColumn]), nl,
+    matrix_get(Board, Row, Column, Checker),
+    format('Checker is ~w', [Checker]), nl,
+    matrix_replace(Board, Row, Column, empty, MiddleBoard),
+    matrix_replace(MiddleBoard, MoveRow, MoveColumn, Checker, NewBoard).
